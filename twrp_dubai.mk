e35faf7b08b43c06bf6dd153fa63a40930a79bc9
Matheus de Jesus <matheusgcdj@gmail.com>
2025-02-12 15:51:46 -0300

    Update and rename omni_dubai.mk to twrp_dubai.mk

--- twrp_dubai.mk
+++ twrp_dubai.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from dubai device
 $(call inherit-product, device/motorola/dubai/device.mk)
@@ -18,7 +18,7 @@ $(call inherit-product, device/motorola/dubai/device.mk)
 PRODUCT_DEVICE := dubai
 PRODUCT_NAME := omni_dubai
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g200 5G
+PRODUCT_MODEL := moto edge 30
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
